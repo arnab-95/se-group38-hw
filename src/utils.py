f47938d0ed09395fb31551a668434e984ef02c29
dakshilkanakia <kanakiadakshil@gmail.com>
2022-09-05 19:34:11 -0400

    Uncommented the testcases

--- src/utils.py
+++ src/utils.py
@@ -39,20 +39,17 @@ def rnd(x,places):
         mult = 10**places
     return (math.floor(x * mult +0.5)/mult)
 
+# TODO: Fix the code
 def coerce(s):
-    if s == 'true':
-        return True
-    if s == 'false':
-        return False
-    def fun(s1):
-        if s1 == 'true':
-            return True
-        if s1 == 'false':
-            return False
-    if type(s) == str:
-        if s.isdigit() == True:
-            s = int(s)
-    return s or int(s) or s
+    pass
+    # if s == 'true':
+    #     return True
+    # if s == 'false':
+    #     return False
+    # if type(s) == str:
+    #     if s.isdigit() == True:
+    #         s = int(s)
+    # return s or int(s) or s
 
 def per(t,p):
     p = math.floor((( p or .5)*len(t))+.5)
